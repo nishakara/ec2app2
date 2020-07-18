d34bfab20d6fb5e33d02522446b2f0b8e923846b
nishakara <nishakara.ac@gmail.com>
2020-07-18 18:38:43 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello Gaya
 
           
         </p>
         <p>
-        Hi There</p>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
